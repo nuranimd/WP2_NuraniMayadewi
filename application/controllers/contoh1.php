8eb8af679e62c86014bbd3bea952fed264cacd1f
nuranimd <nuranijimeansvjn1912@gmail.com>
2021-09-27 22:35:54 +0700

    Upload Tugas Pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h>";
+        echo "Nama saya Nurani Mayadewi
+            Saya tinggal di daerah Bekasi
+            Olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
